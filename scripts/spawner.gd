17e1b8a8efd5c0ad16b016df16c2eda84d5284a4
Sophie <sophie.thi.nguyen11@gmail.com>
2026-07-11 12:10:18 -0700

    spawner thing

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -4,11 +4,21 @@ var whirlpool_scene = preload("uid://2h64y5eyev36")
 @onready var obstacles: Node2D = $"../obstacles"
 @onready var viewport = get_viewport().get_visible_rect().size
 
+
+# chunk related stuff
+var chunks_traveled : int = 0
+# should be fine, but I want it to be the size of the camera
+@onready var chunk_length: int = viewport.x / 2
+
 func _ready() -> void:
 	for i in range(5):
 		spawn()
 
-
+func _process(delta: float) -> void:
+	if int(position.x) % chunk_length == 0:
+		# modulo = 0
+		print("modulo")
+		chunks_traveled += 1
 func spawn():
 	var new_whirlpool = whirlpool_scene.instantiate()
 	
