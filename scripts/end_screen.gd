3bd39373a7eba25e25208474e74dcb083fc4060c
Jasper-Cn <leshan.jasper@gmail.com>
2026-07-11 16:50:53 -0700

    score final change

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -7,7 +7,10 @@ func _ready():
 	EventBus.game_over.connect(gameover)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	score.text = "Your score is " + str(int(Global.score))
+	if Global.measure_unit == "yards":
+		score.text = "Your score is " + str(int(Global.score))
+	else:
+		score.text = "Your score is " + str(int(Global.score*0.9144))
 	if Input.is_action_just_pressed("restart game") and not Global.game_is_running:
 		Global.start_game(true)
 	if Global.game_is_running:
