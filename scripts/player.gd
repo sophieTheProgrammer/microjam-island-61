15e9a6cc97117a88512be115459037ec3879454b
Jasper-Cn <leshan.jasper@gmail.com>
2026-07-11 12:49:15 -0700

    more player shtuffs

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,16 +8,15 @@ const buffer = 100
 var direction = 0
 func _ready() -> void:
 	velocity.x = 0
-func _physics_process(_delta: float) -> void:
-	var viewport := get_viewport_rect().size
+func _physics_process(delta: float) -> void:
 	direction = Input.get_axis("up", "down")
 	velocity.y = move_toward(velocity.y, SPEED * direction * y_multiplier, acceleration * y_multiplier)
 	velocity.x = move_toward(velocity.x, SPEED, acceleration)
-	if position.y + buffer > viewport.y/2:
+	if position.y + buffer > Global.viewport.y/2:
 		velocity.y = 0
-		position.y = viewport.y/2 - buffer
-	elif position.y - buffer < -viewport.y/2:
+		position.y = Global.viewport.y/2 - buffer
+	elif position.y - buffer < -Global.viewport.y/2:
 		velocity.y = 0
-		position.y = -viewport.y/2 + buffer
-	self.rotation = direction * 45
+		position.y = -Global.viewport.y/2 + buffer
+	self.rotation = lerp_angle(self.rotation, direction*45, 10 * delta)
 	move_and_slide()
