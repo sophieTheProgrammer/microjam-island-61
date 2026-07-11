3df13d0b4045e8b0e1dff83420c9fd04123b0f35
Sophie <sophie.thi.nguyen11@gmail.com>
2026-07-11 14:13:16 -0700

    whirlpool animation!

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 func spawn_chunk():
 	var new_chunk = chunk.instantiate()
 	new_chunk.position.x = (chunks_spawned * Global.viewport.x)
-	self.add_child(new_chunk)
+	self.add_child.call_deferred(new_chunk)
 	chunks_spawned+=1
 	
 	
