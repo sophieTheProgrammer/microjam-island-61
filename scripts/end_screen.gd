6f43f4f93173b2c7d6261b25f5be5ab511bba06b
Sophie <sophie.thi.nguyen11@gmail.com>
2026-07-11 15:24:24 -0700

    basic dying

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
 @onready var score: RichTextLabel = $CanvasLayer/score
-var game: PackedScene = preload("res://scenes/game.tscn")
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	score.text = "Your score is " + str(Global.score)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	EventBus.game_over.connect(gameover)
+	score.text = "Your score is " + str(int(Global.score))
 	if Input.is_action_just_pressed("restart game"):
-		get_tree().change_scene_to_packed(game)
+		Global.start_game()
+		
+
+func gameover():
+	$CanvasLayer.show()
+	Engine.time_scale = 0
