a6498cae549ea6caa38c7d9b46bd97115f83a32f
Jasper-Cn <leshan.jasper@gmail.com>
2026-07-11 15:27:29 -0700

    bug fix?

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 @onready var score: RichTextLabel = $CanvasLayer/score
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _ready():
 	EventBus.game_over.connect(gameover)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
 	score.text = "Your score is " + str(int(Global.score))
 	if Input.is_action_just_pressed("restart game"):
 		Global.start_game()
