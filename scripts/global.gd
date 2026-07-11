9c360866835588d4794b83f3a5b56ce4c7d12dd6
Sophie <sophie.thi.nguyen11@gmail.com>
2026-07-11 16:21:58 -0700

    death things

--- scripts/global.gd
+++ scripts/global.gd
@@ -4,21 +4,32 @@ const START = preload("res://scenes/start.tscn")
 const GAME = preload("res://scenes/game.tscn")
 const SETTINGS = preload("res://scenes/settings.tscn")
 const TUTORIAL = preload("res://scenes/cutscene_tutorial.tscn")
+const WORLD_CONTAINER = preload("res://scenes/world_container.tscn")
 
 @onready var viewport = get_viewport().get_visible_rect().size
-#@onready var camera = get_tree().$player/Camera2D
+@onready var world_container_node = get_tree().current_scene.get_node("world_container")
 var player_node : CharacterBody2D
 var fade_node : CanvasLayer
+
+const DEBUG_DEATH : bool = false
+
+var game_running: bool = false
 
 func _process(_delta: float) -> void:
 	viewport = get_viewport().get_visible_rect().size
 var score = 0
 
-func start_game():
+func start_game(reloadWorld):
+	game_running = true
 	Engine.time_scale = 1
-	#fade_node.fade(1,0)
-	get_tree().change_scene_to_packed(GAME)
-
-	#await fade_node.fade(0,1.5).finished
-	get_tree().reload_current_scene()
-	
+	await fade_node.fade(1, 1.5).finished
+	world_container_node.queue_free()
+	if reloadWorld:
+		var world_container = WORLD_CONTAINER.instantiate()
+		world_container_node = world_container
+		get_tree().current_scene.add_child(world_container)
+		await fade_node.fade(0,1.5).finished
+	else:
+		get_tree().change_scene_to_packed(GAME)
+		await fade_node.fade(0,1.5).finished
+		
