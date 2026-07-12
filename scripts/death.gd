f7d7a763b1b17586a9e91de9a00a8081f2213e0c
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2026-07-11 20:50:21 -0700

    looks at whirlpool

--- scripts/death.gd
+++ scripts/death.gd
@@ -6,9 +6,11 @@ func _on_body_entered(_body: Node2D) -> void:
 	if !Global.DEBUG_DEATH:
 		if is_whirlpool:
 			var tween = create_tween()
+			_body.look_at(Vector2(global_position))
 			tween.set_parallel(true)
 			tween.tween_property(_body, "scale", Vector2(0,0),1)
 			tween.tween_property(_body, "position", global_position,1)
+			
 			print("tweening")
 		print("dying shall commence now")
 		EventBus.game_over.emit()
