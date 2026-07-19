9ac0f66edb749c34da381b473b9322ee7a3c9ca0
Sophie <sophie.thi.nguyen11@gmail.com>
2026-07-19 12:26:35 -0700

    break up player logic into functions

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,47 +12,37 @@ const ACCELERATION = 6
 const BUFFER = 75
 var log_ref = null
 var direction = 0
+
+enum player_state {
+	IDLE,
+	SWIMMING,
+	ON_LOG,
+	DEAD
+}
+
+var state
+
 func _ready() -> void:
 	Global.player_node = self
 	EventBus.player_attaches_to_log.connect(attach_to_log)
 	EventBus.deattached_log.connect(detach_from_log)
+	
+	state = player_state.IDLE
 	velocity.x = 0
 	beach_sprite.show()
 	
+	
 func _physics_process(delta: float) -> void:
 	if not Global.game_is_running:
-		cpu_particles_2d.hide()
-		animated_sprite_2d.stop()
-		beach_sprite.show()
-		animated_sprite_2d.frame = 0
-		animated_sprite_2d.hide()
-		Global.is_decreasing = false
-		Global.attached = false
+		# if dead
+		handle_death()
 	else:
 		if Global.attached and Global.game_is_running:
-				self.position.x = move_toward(self.position.x, log_ref.position.x - 70, 10)
-				self.position.y = move_toward(self.position.y, log_ref.position.y, 10)
-				if Input.is_action_just_pressed("escapethelog"):
-					detach_from_log()
-				direction = 0
-				velocity = Vector2(100, 0)
-				animated_sprite_2d.play("log")
-			#log_ref.velocity = self.velocity
+			# if on log
+			handle_log()
 		else:
-			animated_sprite_2d.show()
-			beach_sprite.hide()
-			animated_sprite_2d.play("default")
-			cpu_particles_2d.show()
-			direction = Input.get_axis("up", "down")
-			velocity.y = move_toward(velocity.y, SPEED * direction * Y_MULT, ACCELERATION * Y_MULT)
-			velocity.x = move_toward(velocity.x, SPEED, ACCELERATION)
-			if position.y + BUFFER > Global.viewport.y/2:
-				velocity.y = 0
-				position.y = Global.viewport.y/2 - BUFFER
-			elif position.y - BUFFER < -Global.viewport.y/2:
-				velocity.y = 0
-				position.y = -Global.viewport.y/2 + BUFFER
-			move_and_slide()
+			# swimming
+			handle_swimming()
 		self.rotation = lerp_angle(self.rotation, direction*45, 10 * delta)
 
 
@@ -72,3 +62,37 @@ func detach_from_log():
 	cpu_particles_2d.show()
 	Global.attached = false
 	animated_sprite_2d.play()
+	
+func handle_log():
+	self.position.x = move_toward(self.position.x, log_ref.position.x - 70, 10)
+	self.position.y = move_toward(self.position.y, log_ref.position.y, 10)
+	if Input.is_action_just_pressed("escapethelog"):
+		detach_from_log()
+	direction = 0
+	velocity = Vector2(100, 0)
+	animated_sprite_2d.play("log")
+	
+func handle_death():
+	cpu_particles_2d.hide()
+	animated_sprite_2d.stop()
+	beach_sprite.show()
+	animated_sprite_2d.frame = 0
+	animated_sprite_2d.hide()
+	Global.is_decreasing = false
+	Global.attached = false
+
+func handle_swimming():
+	animated_sprite_2d.show()
+	beach_sprite.hide()
+	animated_sprite_2d.play("default")
+	cpu_particles_2d.show()
+	direction = Input.get_axis("up", "down")
+	velocity.y = move_toward(velocity.y, SPEED * direction * Y_MULT, ACCELERATION * Y_MULT)
+	velocity.x = move_toward(velocity.x, SPEED, ACCELERATION)
+	if position.y + BUFFER > Global.viewport.y/2:
+		velocity.y = 0
+		position.y = Global.viewport.y/2 - BUFFER
+	elif position.y - BUFFER < -Global.viewport.y/2:
+		velocity.y = 0
+		position.y = -Global.viewport.y/2 + BUFFER
+	move_and_slide()
