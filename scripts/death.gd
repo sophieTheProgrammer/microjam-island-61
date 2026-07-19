593a9ccc8806b3b937123e3d6fd6ab8404debad0
Sophie <sophie.thi.nguyen11@gmail.com>
2026-07-19 14:07:54 -0700

    more cleanup

--- scripts/death.gd
+++ scripts/death.gd
@@ -3,20 +3,21 @@ extends Area2D
 @export var is_whirlpool = false
 @export var is_shark = false
 func _on_body_entered(body: Node2D) -> void:
-	if !Global.DEBUG_DEATH:
-		if is_whirlpool:
-			var tween = create_tween()
-			body.look_at(Vector2(global_position))
-			tween.set_parallel(true)
-			tween.tween_property(body, "scale", Vector2(0,0),1)
-			tween.tween_property(body, "position", global_position,1)
-			print("tweening")
-			AudioPlayer.play_sfx(AudioPlayer.SPLASH, -1)
-			EventBus.game_over.emit("whirlpool")
-		if is_shark:
-			EventBus.game_over.emit("shark")
-			AudioPlayer.play_sfx(AudioPlayer.CHOMP, -1)
-		print("dying shall commence now")
+	if Global.DEBUG_DEATH:
+		return
+	if is_whirlpool:
+		var tween = create_tween()
+		body.look_at(Vector2(global_position))
+		tween.set_parallel(true)
+		tween.tween_property(body, "scale", Vector2(0,0),1)
+		tween.tween_property(body, "position", global_position,1)
+		print("tweening")
+		AudioPlayer.play_sfx(AudioPlayer.SPLASH, -1)
+		EventBus.game_over.emit("whirlpool")
+	if is_shark:
+		EventBus.game_over.emit("shark")
+		AudioPlayer.play_sfx(AudioPlayer.CHOMP, -1)
+	print("dying shall commence now")
 
 
 
--- scripts/player.gd
+++ scripts/player.gd
@@ -13,57 +13,54 @@ const BUFFER = 75
 var log_ref = null
 var direction = 0
 
-enum player_state {
+enum PlayerState {
 	BEACH,
 	SWIMMING,
 	ON_LOG,
 	DEAD
 }
 
-var state : player_state = player_state.BEACH
+var state : PlayerState = PlayerState.BEACH
 
 func _ready() -> void:
 	Global.player_node = self
 	EventBus.player_attaches_to_log.connect(attach_to_log)
 	EventBus.deattached_log.connect(detach_from_log)
-	state = player_state.BEACH
+	state = PlayerState.BEACH
 	
 	
 func _physics_process(delta: float) -> void:
 	if not Global.game_is_running:
 		# if dead
-		state = player_state.DEAD
+		state = PlayerState.DEAD
 		#handle_death()
 	else:
-		if not state == player_state.ON_LOG:
-			state = player_state.SWIMMING
+		if not state == PlayerState.ON_LOG:
+			state = PlayerState.SWIMMING
 		self.rotation = lerp_angle(self.rotation, direction*45, 10 * delta)
 	match state:
-		player_state.BEACH:
+		PlayerState.BEACH:
 			handle_beach()
-		player_state.SWIMMING:
+		PlayerState.SWIMMING:
 			handle_swimming()
-		player_state.ON_LOG:
+		PlayerState.ON_LOG:
 			handle_log()
-		player_state.DEAD:
+		PlayerState.DEAD:
 			handle_death()
 	#print(state)
-func attach_to_log(log_ref):
-	state = player_state.ON_LOG
-	print("attaching shall commence")
-	AudioPlayer.play_sfx(AudioPlayer.LOG, 1)
-	cpu_particles_2d.hide()
-	self.log_ref = log_ref
-	animated_sprite_2d.stop()
-	animated_sprite_2d.frame = 3
-	
 
-func detach_from_log():
-	state = player_state.SWIMMING
-	AudioPlayer.play_sfx(AudioPlayer.LOG, 6767)
-	EventBus.player_deattaches_to_log.emit()
-	cpu_particles_2d.show()
-	animated_sprite_2d.play()
+func handle_swimming():
+	handle_swimming_visuals()
+	direction = Input.get_axis("up", "down")
+	velocity.y = move_toward(velocity.y, SPEED * direction * Y_MULT, ACCELERATION * Y_MULT)
+	velocity.x = move_toward(velocity.x, SPEED, ACCELERATION)
+	if position.y + BUFFER > Global.viewport.y/2:
+		velocity.y = 0
+		position.y = Global.viewport.y/2 - BUFFER
+	elif position.y - BUFFER < -Global.viewport.y/2:
+		velocity.y = 0
+		position.y = -Global.viewport.y/2 + BUFFER
+	move_and_slide()
 	
 func handle_log():
 	Global.player_is_swimming = false
@@ -75,33 +72,39 @@ func handle_log():
 	velocity = Vector2(100, 0)
 	animated_sprite_2d.play("log")
 	
+func attach_to_log(log_ref):
+	print("attaching to log")
+	state = PlayerState.ON_LOG
+	AudioPlayer.play_sfx(AudioPlayer.LOG, 1)
+	cpu_particles_2d.hide()
+	self.log_ref = log_ref
+	animated_sprite_2d.stop()
+	animated_sprite_2d.frame = 3
+	
+func detach_from_log():
+	state = PlayerState.SWIMMING
+	AudioPlayer.play_sfx(AudioPlayer.LOG, 6767)
+	EventBus.player_deattaches_to_log.emit()
+	cpu_particles_2d.show()
+	animated_sprite_2d.play()
+	
 func handle_death():
+	# Logic
 	Global.player_is_swimming = false
+	Global.is_decreasing = false
+	# Visuals
 	cpu_particles_2d.hide()
 	animated_sprite_2d.stop()
 	animated_sprite_2d.frame = 0
 	animated_sprite_2d.hide()
-	Global.is_decreasing = false
-
-func handle_swimming():
-	Global.player_is_swimming = true
-	animated_sprite_2d.show()
-	beach_sprite.hide()
-	animated_sprite_2d.play("default")
-	cpu_particles_2d.show()
-	
-	direction = Input.get_axis("up", "down")
-	velocity.y = move_toward(velocity.y, SPEED * direction * Y_MULT, ACCELERATION * Y_MULT)
-	velocity.x = move_toward(velocity.x, SPEED, ACCELERATION)
-	if position.y + BUFFER > Global.viewport.y/2:
-		velocity.y = 0
-		position.y = Global.viewport.y/2 - BUFFER
-	elif position.y - BUFFER < -Global.viewport.y/2:
-		velocity.y = 0
-		position.y = -Global.viewport.y/2 + BUFFER
-	move_and_slide()
 
 func handle_beach():
 	velocity.x = 0
 	beach_sprite.show()
 	animated_sprite_2d.hide()
+func handle_swimming_visuals():
+	Global.player_is_swimming = true
+	animated_sprite_2d.show()
+	beach_sprite.hide()
+	animated_sprite_2d.play("default")
+	cpu_particles_2d.show()
